3f1e8a4dd8e09fc58ea167daa1c0f16ab86a643e
Suji Park <psjhero94@naver.com>
2025-03-21 14:26:19 +0900

    스켈레톤 모바일 적용방식 수정

--- src/components/shared/Skeleton.ts
+++ src/components/shared/Skeleton.ts
@@ -19,19 +19,27 @@ const opacity = keyframes`
 const Skeleton = styled.div<{
   width: string | number;
   height: string | number;
-}>(({ width, height }) => ({
-  width,
-  height,
-  backgroundColor: colors.gray100,
-  animation: `${opacity} 2s ease-in-out 0.5s infinite`,
-}));
+}>(({ width, height }) => {
+  const mobileWidth = typeof width === "number" ? `${(width * 2) / 3}px` : width;
+  const mobileHeight = typeof height === "number" ? `${(height * 2) / 3}px` : height;
+
+  return {
+    width,
+    height,
+    backgroundColor: colors.gray100,
+    animation: `${opacity} 2s ease-in-out 0.5s infinite`,
+    "@media (max-width: 480px)": {
+      width: mobileWidth,
+      height: mobileHeight,
+    },
+  };
+});
 
 export default Skeleton;
-
 
 /**
  * <Skeleton> 컴포넌트
  * - 로딩 중에 UI의 자리 차지(스켈레톤 UI)
- * 
+ *
  * <Skeleton width="100px" height="20px" />
  */
